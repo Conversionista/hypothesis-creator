3f60b1e33ab884b9e8556202c6b37eaa824c680d
Simon Dahla <simon@conversionista.se>
2016-06-07 20:46:19 +0200

    version 1.5

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,138 +1,151 @@
-var hEx = {
-	0 : 'the users doesn\'t utilize the buttons on the site',
-	1 : 'looking at the number of clicks in Google Analytics',
-	2 : 'make the buttons more "clickable" by adding drop shadows',
-	3 : 'all mobile users',
-	4 : 'a better understanding of what constitutes a button on the page',
-	5 : 'higher CTR to other pages and more sales'
-};
+/* global $, WebFont */
 
-$('#hExample').click(function(e) {
-
-	$('input').each(function(index){
-    	$(this).val(hEx[index]);
-	});
-
-	$('#generate').prop('disabled', false);
-
+WebFont.load({
+    typekit: { id: 'fvc2tgi' }
 });
 
-$('#h').on('show.bs.modal', function (event) {
-	var fields = $('input').serializeArray();
 
+function showExample(){
+	'use strict';
+
+	var exampleText = [
+		'the users doesn\'t utilize the buttons on the site',
+		'looking at the number of clicks in Google Analytics',
+		'make the buttons more "clickable" by adding drop shadows',
+		'all mobile users',
+		'a better understanding of what constitutes a button on the page',
+		'higher CTR to other pages and more sales'
+	];
+	var form = $('#hypothesisForm').serializeArray();
+	$.each(form, function(i, v) {
+		$('form [name="' + v.name + '"]').val(exampleText[i]);
+		// $(v).val(exampleText[i]);
+	});
+
+}
+
+function generateHypothesis(){
+	var fields = $('#hypothesisForm').serializeArray();
+	'use strict';
 	var hypoEN = '<b>Since we have observed that</b> ' + fields[0].value + ' <b>by</b> ' + fields[1].value + '.<br /><br /> <b>We want to</b> ' + fields[2].value + ' <b>for</b> ' + fields[3].value + '.<br /><br /><b>Which should lead to</b> ' + fields[4].value + ' <b>and the effect will be measured by</b> ' + fields[5].value + '.';
+	$('p.output').html(hypoEN);
+	$('textarea.output').text($('p.output').text());
+	$('.hide').removeClass('hide');
+}
 
-	$('.modal-body').empty().append('<p>'+hypoEN+'</p>');
 
-	var copyData = $('.modal-body').text();
-	var client = new ZeroClipboard( document.getElementById('copy_button') );
-
-	// console.log(copyData);
-
-	ZeroClipboard.setData('text/plain', copyData);
-
-	client.on( 'ready', function( readyEvent ) {
-		// console.log( 'ZeroClipboard SWF is ready!' );
-
-		client.on( 'aftercopy', function( event ) {
-			// `this` === `client`
-			// `event.target` === the element that was clicked
-			// event.target.style.display = "none";
-			$(event.target).text('Copied!');
-			// console.log('Copied text to clipboard: ' + event.data['text/plain'] );
-		} );
-	} );
-});
-
-$('#generate').prop('disabled', true);
-
-$('input').on('input keyup change', function(e) {
+$('input').on('blur', function(event) {
+	event.preventDefault();
+	/* Act on the event */
+	var filledOut = [];
+	var input = $('#hypothesisForm input');
 	
-	var x = [];
-	var y = 0;
-
-	$('input').each(function(index){
-		if($(this).val().length > 0){
-	        x.push(1);
-	    }else{
-	        x.push(0);
-	    }
+	input.each(function(index, el) {
+		
+		if($(this).val() !== ''){
+			filledOut.push(true);
+		} else {
+			filledOut.push(false);
+		}
+		
 	});
+	console.log(filledOut);
+	console.log($.inArray(false, filledOut));
 
-	for(var i in x) { y += x[i]; }
-	
-	if(y === 6){
-		$('#generate').prop('disabled', false);
+	if($.inArray(false, filledOut) === -1){
+		console.log('yay!');
+		$('#generate').attr('disabled', false);
 	} else {
-		$('#generate').prop('disabled', true);
+		$('#generate').attr('disabled', true);
 	}
 
+	// if($.inArray(false, filledOut)){
+	// 	console.log(false);
+	// } else {
+	// 	console.log(true);
+	// }
 });
 
-$(document).ready(function() {
-	$('#hypothesisForm').bootstrapValidator({
-		container: 'tooltip',
-		
-	    excluded: [':disabled', ':hidden', ':not(:visible)'],
-	    feedbackIcons: 'disabled',
-        fields: {
-         	problemDescription: {
-         		threshold: 0,
-        		trigger: 'keyup blur',
-	            validators: {
-	                // notEmpty: {
-	                //     message: 'The summary is required'
-	                // }
-	            }
-	        },
-	        observationMethod: {
-	        	threshold: 0,
-        		trigger: 'keyup blur',
-	            validators: {
-	                // notEmpty: {
-	                //     message: 'The summary is required'
-	                // }
-	            }
-	        },
-	        makeThisChange: {
-	        	threshold: 0,
-        		trigger: 'keyup blur',
-	            validators: {
-	                // notEmpty: {
-	                //     message: 'The summary is required'
-	                // }
-	            }
-	        },
-	        forThisSegment: {
-	        	threshold: 0,
-        		trigger: 'keyup blur',
-	            validators: {
-	                // notEmpty: {
-	                //     message: 'The summary is required'
-	                // }
-	            }
-	        },
-	        behaviour: {
-	        	threshold: 0,
-        		trigger: 'keyup blur',
-	            validators: {
-	                // notEmpty: {
-	                //     message: 'The summary is required'
-	                // }
-	            }
-	        },
-	        kpi: {
-	        	threshold: 0,
-        		trigger: 'keyup blur',
-	            validators: {
-	                // notEmpty: {
-	                //     message: 'The summary is required'
-	                // }
-	            }
-	        }
-	    },
-	    live: 'enabled',
-	    message: 'This value is not valid',
-	    submitButtons: '#generate'
+function getGACount() {
+	$.ajax({
+		url: 'https://api.conversionista.se/hypothesis-generator-count/',
+		type: 'GET'
+	})
+	.done(function(data) {
+		var count = data.events;
+			count = parseInt(count);
+			count = 1752 + count;
+		console.log(count);
+
+		if(count !== 0){
+			console.log(count)
+			$('header h3').html('Over <b id="gaCount">' + count + '</b> hypothesis created with this tool so far.');
+			// $('header.row').append('<h3>Over <b id="gaCount">' + count + '</b> hypothesis created with this tool so far.</h3>');
+			var o = {
+			  useEasing : true, 
+			  useGrouping : true, 
+			  separator : ' ', 
+			  decimal : '.', 
+			  prefix : '', 
+			  suffix : '' 
+			};
+			var c = new CountUp('gaCount', 0, count, 0, 1, o);
+			c.start();
+		} else {
+			console.log('no count found...')
+		}
+
 	});
+}
+
+$('.copy').on('click', function(event) {
+	event.preventDefault();
+	var t = $(this);
+	t.text('Copied!').addClass('animated shake');
+	setTimeout(function(){
+		console.log('copy')
+		t.text('Copy').removeClass('shake');
+	},2500);
 });
+
+
+$(document).ready(function() {
+	'use strict';
+	window.dataLayer = window.dataLayer || [];
+	$('#hExample').click(function(event) {
+		event.preventDefault();
+		$('#generate').attr('disabled', false);
+		showExample();
+
+		window.dataLayer.push({
+			'event' : 'event',
+			'eventCategory' : 'Hypothesis Generator',
+			'eventAction' : 'Click',
+			'eventLabel' : 'Show Example',
+			'eventNonInteraction' : false
+		});
+
+		generateHypothesis();
+	});
+
+	$('#generate').click(function(event) {
+		event.preventDefault();
+		generateHypothesis();
+		
+		window.dataLayer.push({
+			'event' : 'event',
+			'eventCategory' : 'Hypothesis Generator',
+			'eventAction' : 'Click',
+			'eventLabel' : 'Generate',
+			'eventNonInteraction' : false
+		});
+	});
+
+	new Clipboard('.btn');
+
+	getGACount();
+
+});
+
+
+
